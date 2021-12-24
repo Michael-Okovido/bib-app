8da427444ea819da2dea168048af423d1b082612
Michael-Okovido <michaelokovidoc@gmail.com>
2021-12-24 17:18:44 +0100

    router working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import App from "./views/Home.vue";
+import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import router from "./router";
